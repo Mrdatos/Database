b14ac685bda8c134a74c504e0482b6cfaddfa340
Mrdatos <hurtadodanieldavid@gmail.com>
2025-12-01 03:45:10 +0000

    actualizacion

--- analisis_datos.py
+++ analisis_datos.py
@@ -1,2 +1,12 @@
-# selectAnálisis de datos
-print("soy el mejor")
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.title('mi prueba')
+ventana.geometry('300x200')  # Define tamaño de la ventana
+
+tk.Label(ventana, text='hola mundo', font=('Arial', 16)).pack(pady=20)
+tk.Button(ventana, text='Cerrar', command=ventana.destroy).pack()
+
+ventana.mainloop()
+
+
